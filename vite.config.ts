855347ca414dc91edd088b22c6b289ee7a639c52
Nasiali-Eden <nasieden0@gmail.com>
2026-01-10 01:51:46 +0300

    newest

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/shield-web-app/',
+  build: {
+    outDir: 'docs',     // <-- build goes directly to docs/
+  },
 })
